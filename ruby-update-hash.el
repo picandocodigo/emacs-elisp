2506b1355bb824a04ce611d97a994e8ee56ac3a9
Fernando Briano <fernando@picandocodigo.net>
2013-03-12 17:48:55 -0300

    Fixes typo

--- ruby-update-hash.el
+++ ruby-update-hash.el
@@ -2,7 +2,7 @@
   (interactive)
 
   (save-excursion
-    (goto-char (poing-min))
+    (goto-char (point-min))
     "Update Ruby hash syntax to 1.9.3"
     (while (search-forward-regexp "\\(:\\)\\(\\w*\\)\\s-*=>" nil t)
       (replace-match "\\2\\1" t nil))
